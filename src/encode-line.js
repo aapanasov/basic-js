3056ee97112514bc1538705f24e6d37953a61263
aapanasov <aapanasov@gmail.com>
2022-04-30 17:26:39 +0300

    feat: solve encode-line

--- src/encode-line.js
+++ src/encode-line.js
@@ -1,4 +1,4 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const {NotImplementedError} = require('../extensions/index.js');
 
 /**
  * Given a string, return its encoding version.
@@ -10,10 +10,35 @@ const { NotImplementedError } = require('../extensions/index.js');
  * For aabbbc should return 2a3bc
  *
  */
-function encodeLine(/* str */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function encodeLine(str) {
+  const arr = []
+  let result = ''
+  let counter = 1;
+
+  for (let i = 0; i < str.length; i++) {
+    if (str[i] === str[i + 1]) {
+      counter++;
+    } else {
+      arr.push([counter, str[i]]);
+      counter = 1;
+    }
+  }
+
+  for (let pair of arr) {
+    if (pair[0] > 1) {
+      result += pair[0] + pair[1]
+    } else {result += pair[1]}
+  }
+
+  return result
 }
+
+
+
+
+
+
+console.log(encodeLine('abbcca'))
 
 module.exports = {
   encodeLine
